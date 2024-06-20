330b9c3975ecbf8142ad17aab9006a99fb6f21a2
Lucas Sabino <112620334+Efledoscorre@users.noreply.github.com>
2024-06-20 18:42:04 -0300

    Add files via upload

---
+++ src/main/java/ucb/rpg/visual/ApresentacaoArco.java
@@ -0,0 +1,68 @@
+package ucb.rpg.visual;
+
+public class ApresentacaoArco {
+
+    public static void main(String[] args) {
+        mostrarArco();
+    }
+
+    public static void mostrarArco() {
+        System.out.println("                                                                          \r\n"
+        		+ "                                                                                                    \r\n"
+        		+ "                                                                                                    \r\n"
+        		+ "                                                                                                    \r\n"
+        		+ "                                                                                                    \r\n"
+        		+ "                                  ..                                                                \r\n"
+        		+ "                                  :==-..                                                            \r\n"
+        		+ "                                  .-=:.                                                             \r\n"
+        		+ "                                  ..:=-:..                                                          \r\n"
+        		+ "                                  ...:-==--:..                                                      \r\n"
+        		+ "                                  .. ..:-====--:..                                                  \r\n"
+        		+ "                                  ..    ...:-=====-.                                                \r\n"
+        		+ "                                  ..         ..-====-..                                             \r\n"
+        		+ "                                  ..           .:-===-:.                                            \r\n"
+        		+ "                                  ..             .-====-.                                           \r\n"
+        		+ "                                  ..              .:====-.                                          \r\n"
+        		+ "                                  ..              .-=====-.                                         \r\n"
+        		+ "                                  ..              -===-.                                            \r\n"
+        		+ "                                  ..             .-====-...                                         \r\n"
+        		+ "                                  ..            .-========-:..                                      \r\n"
+        		+ "                                  ..            .:=====+++===-:..                                   \r\n"
+        		+ "                                  ..             .-=*#######*+=-..                                  \r\n"
+        		+ "                                  ..              .:*###*-...                                       \r\n"
+        		+ "                                  ..               :*##+.                                           \r\n"
+        		+ "                                  ..               :*#+.                                            \r\n"
+        		+ "                                  ..               :*#=                                             \r\n"
+        		+ "                                  ..               :*#+.                                            \r\n"
+        		+ "                                  ..               :*##+.                                           \r\n"
+        		+ "                                  ..               :*###*-.                                         \r\n"
+        		+ "                                  ..             .:=*#######*+=:..                                  \r\n"
+        		+ "                                  ..            .:=====+++===--:.                                   \r\n"
+        		+ "                                  ..            .-========-:..                                      \r\n"
+        		+ "                                  ..             .-====-:..                                         \r\n"
+        		+ "                                  ..              -===-.                                            \r\n"
+        		+ "                                  ..              .-=====-.                                         \r\n"
+        		+ "                                  ..              .:====-.                                          \r\n"
+        		+ "                                  ..             .-====-.                                           \r\n"
+        		+ "                                  ..           .:-===-:.                                            \r\n"
+        		+ "                                  ..         ..-====-..                                             \r\n"
+        		+ "                                  ..      ..-=====-.                                                \r\n"
+        		+ "                                  .. ..:-====---..                                                  \r\n"
+        		+ "                                  ...:-==--:..                                                      \r\n"
+        		+ "                                  ..:=-:..                                                          \r\n"
+        		+ "                                  .:=:.                                                             \r\n"
+        		+ "                                  .==-..                                                            \r\n"
+        		+ "                                  ..                                                                \r\n"
+        		+ "                                                                                                    \r\n"
+        		+ "                                        ");
+               
+        System.out.println("Apresentação da Arma: Arco");
+        System.out.println("Dano: 35-50");
+        System.out.println("Especialidades: ");
+        System.out.println("- Longo alcance");
+        System.out.println("- Alta precisão");
+        System.out.println("- Ataques rápidos");
+        System.out.println("- Ideal para combate a distância");
+        System.out.println("- Transforma simples flechas em flechas mágicas");
+    }
+}
